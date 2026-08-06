8bdef2b9d77a0d72edf1f9cc3146ddfc7e063151
muhammet safa <safatekin1624@gmail.com>
2026-08-06 17:23:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ def bulten_gonder(message):
             )
             
             response = client.models.generate_content(
-                model='gemini-1.5-flash-latest', 
+                model='gemini-2.0-flash', 
                 contents=ozet_bilgi
             )
             
@@ -139,7 +139,7 @@ def tek_hisse_analiz_et(message):
         )
         
         response = client.models.generate_content(
-            model='gemini-1.5-flash-latest', 
+            model='gemini-2.0-flash', 
             contents=prompt
         )
         
